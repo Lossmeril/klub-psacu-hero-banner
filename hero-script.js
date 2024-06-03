9437cbb0034f866d294247e2032f8af5e59626ef
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-06-03 13:35:33 +0200

    feat: bgs and mobile slider

--- hero-script.js
+++ hero-script.js
@@ -1,9 +1,12 @@
 window.onload = function () {
   const left = document.getElementById("left-side");
+  const slider = document.getElementById("mobile-drag");
 
   const handleMove = (e) => {
     left.style.width = `${(e.clientX / window.innerWidth) * 100}%`;
-    left.classList.remove("introAnimation");
+    slider.style.left = `${(e.clientX / window.innerWidth) * 100}%`;
+    left.classList.remove("intro-animation");
+    slider.classList.remove("slider-intro-animation");
   };
 
   document.onmousemove = (e) => handleMove(e);
